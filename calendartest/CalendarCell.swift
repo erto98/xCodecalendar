3a9b4a1a3e5410bef615e4c9c0355265789af49e
ertogrul coskuner <82051373+erto98@users.noreply.github.com>
2022-08-03 15:24:51 +0100

    test

---
+++ calendartest/CalendarCell.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarCell.swift
+//  calendartest
+//
+//  Created by Erto on 3.08.2022.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell
+{
+    @IBOutlet weak var dayOfMonth: UILabel!
+}
